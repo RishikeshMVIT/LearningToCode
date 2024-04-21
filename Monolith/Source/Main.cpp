6181cafeb666cfab807440b2e63ca042b197ae6d
Rishikesh M <rishikesh.m2021@vitbhopal.ac.in>
2024-04-21 19:58:06 +0530

    Update Main.cpp

--- Monolith/Source/Main.cpp
+++ Monolith/Source/Main.cpp
@@ -14,6 +14,38 @@ typedef F16;
 typedef F32;
 typedef F64;
 
+template <typename T>
+class LinearData
+{
+public:
+	LinearData() : array(nullptr), capacity(0), size(0) {}
+	~LinearData()
+	{
+		delete[] array;
+	}
+
+	virtual void Push(const T& value) = 0;
+	virtual T Pop() = 0;
+
+private:
+	void Resize(size_t newCapacity)
+	{
+		T* newArray = new T[newCapacity];
+		for (size_t i = 0; i < size; ++i)
+		{
+			newArray[i] = array[i];
+		}
+
+		delete[] array;
+		array = newArray;
+		capacity = newCapacity;
+	}
+
+private:
+	T* array;
+	size_t size;
+	size_t capacity;
+};
 
 template<typename T>
 class DynamicArray
@@ -89,6 +121,55 @@ private:
 template <typename T>
 class LinkedList
 {
+
+};
+
+template <typename T, int n>
+class Stack
+{
+public:
+	Stack() : array(nullptr), capacity(0), size(0) {}
+	~Stack()
+	{
+		delete[] array;
+	}
+
+	void Push()
+	{
+	}
+
+	T Pop()
+	{
+
+	}
+
+	T Peek()
+	{
+
+	}
+
+	bool IsEmpty()
+	{
+		return (size == 0) ? true : false;
+	}
+
+	bool IsFull()
+	{
+		return (size >= capacity) ? true : false;
+	}
+
+private:
+	T* array;
+	size_t capacity;
+	size_t size;
+};
+
+template<typename T>
+class Queue
+{
+public:
+
+private:
 
 };
 
